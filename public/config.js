22fd459c025903724f300eae04e4e7c945fd167c
Alexandre-dev01 <alexandre.carvalho@esoftware.uniceplac.edu.br>
2025-12-03 08:35:00 -0300

    Atualizando a URL do Site

--- public/config.js
+++ public/config.js
@@ -1,9 +1,6 @@
 const API_BASE_URL = window.location.hostname.includes("onrender.com")
-  ? "https://sistema-manobrista-homologa-o.onrender.com" // <--- ATENÇÃO: CONFIRA SE É ESSE O SEU LINK
+  ? "https://sistema-manobrista-api-v2.onrender.com"
   : "http://localhost:3000";
 
-console.log("API rodando em:", API_BASE_URL); // Só para ajudar a ver no console
-// ... código anterior ...
-
 console.log("API rodando em:", API_BASE_URL);
-console.log("VERSÃO NOVA DO DEPLOY 2.0"); // <--- Adicione esta linha
+console.log("VERSÃO NOVA DO DEPLOY 2.0");
